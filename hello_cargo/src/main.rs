0066472429d741f2e14655704139189a4b53d3a5
John Freeman <jfreeman08@gmail.com>
2021-08-21 18:44:02 -0500

    Play with enums

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Texas,
+}
+
+struct A {
+    a: u32,
+    b: u32,
+}
+
+struct B(u32, u32,);
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", if 1 == 2 { "equal" } else { "not equal" });
+    let state = if 1 == 2 { UsState::Alaska } else { UsState::Texas };
+    println!("{:?}", state);
+    let x = 5.clone();
+    println!("{}", x);
+    B(1, 2);
+    A{a: 1, b: 2};
 }
